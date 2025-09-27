bb773e74586e3a7e6dba1d12951b7b17cf082af0
raincandy <137185033+lexiscandy2@users.noreply.github.com>
2025-09-27 11:04:34 +0800

    Create soal_1.c

---
+++ soal_1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    float J, T, V;
+    
+    scanf ("%f", &J);
+    scanf ("%f", &T);
+    V = J/T;
+    
+    printf("%.3f", V);
+    //printf double = max "%.3if"
+    
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
+    return 0;
+}
